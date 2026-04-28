adc5b365d889101ecc0f40f49c0e1833e0797cdd
Fathul Hidayat <fahtul.singaparna@gmail.com>
2026-04-28 15:31:26 +0700

    fix: add base path for github pages subpath deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kirara-dev.my.id',
+  site: 'https://kirara02.github.io',
+  base: '/personal-website',
 });
