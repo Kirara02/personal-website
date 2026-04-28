527a941dc03c10c9e81a5d1fa8573b05f0476112
Fathul Hidayat <fahtul.singaparna@gmail.com>
2026-04-28 15:23:44 +0700

    fix: update site url to secure https

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://kirara-dev.my.id',
+  site: 'https://kirara-dev.my.id',
 });
