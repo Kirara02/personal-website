7f1a14537a53d89215cd37d835ae9fc3b0fccac1
Fathul Hidayat <fahtul.singaparna@gmail.com>
2026-04-28 15:53:27 +0700

    feat: migrate to custom subdomain portfolio.kirara-dev.my.id

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kirara02.github.io',
-  base: '/personal-website',
+  site: 'https://portfolio.kirara-dev.my.id',
 });
